044ecaeebdec4cf70cc1c33b020dfcc77bde5c3c
Kalinin Aleksandr Aleksandrovich <AAKalinin@gksm.local>
2023-08-22 12:52:02 +0500

    fix

--- assets/vue-i18n.esm-bundler.e929e5bf.js
+++ assets/vue-i18n.esm-bundler.e929e5bf.js
@@ -1,4 +1,4 @@
-import{h as Dt,e as zt,i as en,o as tn,a as nn,s as an,r as ne,c as ae,d as rn,w as Ke,F as ln,g as $e,f as sn,j as cn,T as un}from"./index.d694113a.js";/*!
+import{h as Dt,e as zt,i as en,o as tn,a as nn,s as an,r as ne,c as ae,d as rn,w as Ke,F as ln,g as $e,f as sn,j as cn,T as un}from"./index.1657ef70.js";/*!
   * shared v9.2.2
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
