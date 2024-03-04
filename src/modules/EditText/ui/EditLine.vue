fde7e6952fbfcf04e7ee65fb3f3e2ff125207441
Kalinin Aleksandr Aleksandrovich <AAKalinin@gksm.local>
2024-03-04 11:12:36 +0500

    fix

--- src/modules/EditText/ui/EditLine.vue
+++ src/modules/EditText/ui/EditLine.vue
@@ -18,17 +18,32 @@ watch(translateInner, (cur) => {
   >
     {{ translateInner || line }}
 
-    <q-menu>
+    <q-popup-edit v-model="translateInner" v-slot="scope">
         <q-input
           autofocus
           autogrow
           dense
-          v-model="translateInner"
-        />
+          v-model="scope.value"
+          :model-value="scope.value"
+          @keydown.enter="scope.set"
+        >
+          <template v-slot:after>
+            <q-btn
+              flat dense color="negative" icon="cancel"
+              @click.stop.prevent="scope.cancel"
+            />
+
+            <q-btn
+              flat dense color="positive" icon="check_circle"
+              @click.stop.prevent="scope.set"
+              :disable="scope.validate(scope.value) === false || scope.initialValue === scope.value"
+            />
+          </template>
+        </q-input>
 
         <p class="q-mt-md">
           {{ line }}
         </p>
-      </q-menu>
+      </q-popup-edit>
   </span>
 </template>
